4e6074f0d50db1da25bf43058bdb9e65841126c3
Rabah Babaci <4rabah@gmail.com>
2020-09-01 15:57:52 +0000

    implement the countUniqueValues in O(n)

---
+++ Main/multiPointersPattern.js
@@ -0,0 +1,16 @@
+function countUniqueValues(arr) {
+    let count = 0;
+    let i = 0;
+    let j = 1;
+
+    while (j < arr.length) {
+        if (arr[j] !== arr[i]) {
+            count++;
+            i = j;
+        }
+        j++;
+    }
+    return i > 0 ? count + 1 : count;
+}
+
+console.log(countUniqueValues([1, 1, 3, 3, 4, 4]))
