467e13413b38f9cbb0115f9b0e6f172072292ce4
Rabah Babaci <4rabah@gmail.com>
2020-09-09 15:21:42 +0000

    refactor isSubsequence

--- Main/isSubsequence.js
+++ Main/isSubsequence.js
@@ -1,11 +1,12 @@
 function isSubsequence(str1, str2) {
+    if (!str1) return true;
+
     let k = 0;
     for (let i = 0; i < str2.length; i++) {
         if (str1[k] === str2[i]) {
             k++;
         }
+        if (k === str1.length) return true;
     }
-    
-    if (k === str1.length) return true;
     return false;
 }
