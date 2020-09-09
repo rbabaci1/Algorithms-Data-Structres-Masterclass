20eb0d787a97cbdd7618fc0ac966fd1d113d9c89
Rabah Babaci <4rabah@gmail.com>
2020-09-09 14:53:29 +0000

    implement the averagePair

--- Main/areThereDuplicates.js
+++ Main/areThereDuplicates.js
@@ -2,7 +2,7 @@ function areThereDuplicates(...args) {
   args.sort()
   
   for (let i = 0; i < args.length - 1; i++) {
-      if (args[i] === args[i+1]) return true;
+      if (args[i] === args[i + 1]) return true;
   }
   return false;
 }
---
+++ Main/averagePair.js
@@ -0,0 +1,16 @@
+function averagePair(arr, target){
+let start = 0, end = arr.length - 1;
+
+while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2;
+
+    if (avg === target) return true;
+    else if (avg < target) {
+        start++;
+    } else {
+        end--;
+    }
+}
+
+return false;
+}
