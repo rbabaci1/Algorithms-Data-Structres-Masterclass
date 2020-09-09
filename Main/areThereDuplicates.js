69883a86afb108a37099a32fb605f920f560443f
Rabah Babaci <4rabah@gmail.com>
2020-09-09 14:28:23 +0000

    implement the areThereDuplicates function

---
+++ Main/areThereDuplicates.js
@@ -0,0 +1,10 @@
+function areThereDuplicates(...args) {
+  args.sort()
+  
+  for (let i = 0; i < args.length - 1; i++) {
+      if (args[i] === args[i+1]) return true;
+  }
+  return false;
+}
+
+console.log(areThereDuplicates(1, 3, 4, 5, 4))
