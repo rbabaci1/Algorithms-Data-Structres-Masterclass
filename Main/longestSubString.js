3e219efa075833e55b790e1ab175d34a5cbcebb9
Rabah Babaci <4rabah@gmail.com>
2020-09-11 03:44:15 +0000

    .

--- Main/longestSubString.js
+++ Main/longestSubString.js
@@ -36,3 +36,21 @@ function findLongestSubstring(s){
 
 
 console.log(findLongestSubstring("thisishowwedoit"))
+
+// function findLongestSubstring(str) {
+//   let longest = 0;
+//   let seen = {};
+//   let start = 0;
+ 
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if (seen[char]) {
+//       start = Math.max(start, seen[char]);
+//     }
+//     // index - beginning of substring + 1 (to include current in count)
+//     longest = Math.max(longest, i - start + 1);
+//     // store the index of the next char so as to not double count
+//     seen[char] = i + 1;
+//   }
+//   return longest;
+// }
--- Main/minSubArrayLen.js
+++ Main/minSubArrayLen.js
@@ -1,5 +1,5 @@
 var minSubArrayLen = function(nums, s) {
-    let left = 0, sum = 0, res = Number.MAX_VALUE;
+    let left = 0, sum = 0, res = Infinity;
     
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
@@ -10,7 +10,36 @@ var minSubArrayLen = function(nums, s) {
             left++;
         }
     }
-    return res === Number.MAX_VALUE ? 0 : res;
+    return res === Infinity ? 0 : res;
 };
 
 console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7))
+
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+ 
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then 
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
+    }
+    // if current window adds up to at least the sum given then
+		// we can shrink the window 
+    else if(total >= sum){
+      minLen = Math.min(minLen, end-start);
+			total -= nums[start];
+			start++;
+    } 
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    else {
+      break;
+    }
+  }
+ 
+  return minLen === Infinity ? 0 : minLen;
+}
