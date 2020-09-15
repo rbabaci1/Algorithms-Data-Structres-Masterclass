924fd51f50c94689cc4d008d0e0fa97c4d6f1c50
Rabah Babaci <4rabah@gmail.com>
2020-09-15 17:27:25 +0000

    implement some recursive function

---
+++ Recursive-Problem-Set/someRecursive.js
@@ -0,0 +1,14 @@
+function someRecursive(arr, cb){
+     let isTrue = false;
+     
+     function helper(arr, cb) {
+        if (arr.length === 1) return cb(arr[0]);
+     
+        isTrue = someRecursive(arr.slice(1), cb);
+        return cb(arr[0]) ? cb(arr[0]) : isTrue;
+     }
+     
+     return helper(arr, cb);
+}
+
+console.log(someRecursive([14, 10, 10], v => v > 10))
