cbf48e2530314dd470e3b52f21a81025dd512898
Rabah Babaci <4rabah@gmail.com>
2020-09-22 16:00:01 +0000

    refactor nestedEvenSum

--- Recursive-Problem-Set/nestedEvenSum.js
+++ Recursive-Problem-Set/nestedEvenSum.js
@@ -1,16 +1,12 @@
-function nestedEvenSum(obj) {
-        let keys = Object.keys(obj);
-        if (keys.length === 0) return 0;
-
-        let current = obj[keys[0]];
-        delete obj[keys[0]];
-
-        if (typeof current === 'object' && current  !== null && !Array.isArray(current)) {
-            return nestedEvenSum(Object.assign({}, current, obj));
-        } else {
-            if (typeof current !== 'number' || current % 2 !== 0) current = 0;
-            return current + nestedEvenSum(obj);
+function nestedEvenSum(obj, sum=0) {
+    for (let key in obj) {
+        if (typeof obj[key] === "object") {
+            sum += nestedEvenSum(obj[key])
+        } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+            sum += obj[key]
         }
+    }
+    return sum;
 }
 
 
@@ -34,5 +30,5 @@ var obj2 = {
   e: {e: {e: 2}, ee: 'car'}
 };
 
-// console.log(nestedEvenSum(obj1)) // 6
-console.log(nestedEvenSum(obj2)) // 10
+console.log(nestedEvenSum(obj1)) // 6
+// console.log(nestedEvenSum(obj2)) // 10
