c75b76c68cb85a3760733491f6ca1e87ac8acae1
Rabah Babaci <4rabah@gmail.com>
2020-10-20 15:49:47 +0000

    refactor insertion sort for less iterations

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -1,6 +1,6 @@
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        for (let end = i; end > 0; end-- ) {
+        for (let end = i; end > 0 && arr[end] < arr[end- 1]; end-- ) {
             if (arr[end] < arr[end - 1]) {
                 [arr[end - 1], arr[end]] = [arr[end], arr[end - 1]]
             }
