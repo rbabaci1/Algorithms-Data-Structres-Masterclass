27513a19af2a668c6341a9c3e8e77b5b60e16e83
Rabah Babaci <4rabah@gmail.com>
2020-10-23 20:45:44 +0000

    optimize the quick sort algo

--- Sorting/quickSort.js
+++ Sorting/quickSort.js
@@ -1,32 +1,33 @@
-function partition(arr, start=0, end=arr.length) {
+function partition(arr, start=0, end=arr.length-1) {
     let pivot = arr[start];
-    let swapIndex = start
+    let swapIndex = start;
 
-    for (let i = start + 1; i < end; i++) {
+    for (let i = start + 1; i <= end; i++) {
         if (pivot > arr[i]) {
             swapIndex++;
             if (i !== swapIndex) {
-              [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]]
+                [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]]
             }
         }
     }
     
     if (start !== swapIndex) {
-      [arr[start], arr[swapIndex]] = [arr[swapIndex], arr[start]]
+        [arr[start], arr[swapIndex]] = [arr[swapIndex], arr[start]]
     }
     
-    return swapIndex
+    return swapIndex;
 }
 
 function quickSort(arr, start=0, end=arr.length) {
-    if (start >= end) return;
-
-    let sortedPivot = partition(arr, start, end)
+    if (start < end) {
+        let sortedPivot = partition(arr, start, end);
+        
+        quickSort(arr, start, sortedPivot - 1)
+        quickSort(arr, sortedPivot + 1, end)
+    }
     
-    quickSort(arr, start, sortedPivot)
-    quickSort(arr, sortedPivot + 1)
-
-    return arr
+    return arr;
 }
 
-console.log(quickSort([4, 8, 1, 3]))
+console.log(quickSort([4, 3, 7, 2, 1, 5, -1, 8, 9, -2, -22]))
+// console.log(quickSort([1, 2, 3, 4, 5, 7]))
