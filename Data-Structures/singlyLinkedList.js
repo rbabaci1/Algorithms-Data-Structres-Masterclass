6d666dbbe059fc01b53c256e640bd631e4bc971f
Rabah Babaci <4rabah@gmail.com>
2020-10-28 23:05:18 +0000

    .

--- Data-Structures/singlyLinkedList.js
+++ Data-Structures/singlyLinkedList.js
@@ -15,14 +15,36 @@ class SinglyLinkedList {
     push(val) {
         let newNode = new Node(val);
 
-        if (!this.tail) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
+        if (this.head) {
             this.tail.next = newNode;
-            this.tail = newNode;
+        } else {
+            this.head = newNode;
         }
+        this.tail = newNode;
         this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let prev = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return prev;
+        }
+
+        let curr = prev.next;
+        while (curr.next) {
+            curr = curr.next;
+            prev = prev.next;
+        }
+
+        this.tail = prev;
+        this.tail.next = null;
+        this.length--;
+        return curr;
     }
 }
 
@@ -30,5 +52,8 @@ list = new SinglyLinkedList();
 
 list.push(10)
 list.push(9)
+// list.push(8)
+// list.push(7)
 
+console.log(list.pop())
 console.log(list.length)
