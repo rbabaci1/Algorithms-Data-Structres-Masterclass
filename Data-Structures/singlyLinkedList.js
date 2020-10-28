3d09f46719cc47849bca3ad41adb8afb0b16a6d3
Rabah Babaci <4rabah@gmail.com>
2020-10-28 17:09:07 +0000

    implement the singly linked list with the push method

---
+++ Data-Structures/singlyLinkedList.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.tail) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+}
+
+list = new SinglyLinkedList();
+
+list.push(10)
+list.push(9)
+
+console.log(list.length)
