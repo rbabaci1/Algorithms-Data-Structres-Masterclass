5022e88fc597da1ab53e6242f5c202ac7739852c
Rabah Babaci <4rabah@gmail.com>
2020-10-29 17:33:41 +0000

    implement the get and set methods

--- Data-Structures/singlyLinkedList.js
+++ Data-Structures/singlyLinkedList.js
@@ -8,7 +8,7 @@ class Node {
 class SinglyLinkedList {
     constructor() {
         this.head = null;
-        this.tail = null;
+        this.tail = this.head;
         this.length = 0;
     }
 
@@ -73,6 +73,30 @@ class SinglyLinkedList {
             this.head = newNode;
         }
         this.length++;
+        return this;
+    }
+
+    // get the node at the given position
+    get(position) {
+        if (position < 0 || position >= this.length) return null;
+
+        let counter = 0, curr = this.head;
+        while (curr) {
+            if (position === counter++) return curr;
+            curr = curr.next;
+        }
+    }
+
+    // set the node at the given position with the new val
+    set(newVal, position) {
+        if (position < 0 || position >= this.length) return null;
+
+        let counter = 0, curr = this.head;
+        while (position !== counter++) {
+            curr = curr.next;
+        }
+        curr.val = newVal;
+        return curr;
     }
 }
 
@@ -80,8 +104,9 @@ list = new SinglyLinkedList();
 
 list.push(10)
 list.push(9)
-// list.push(8)
-// list.push(7)
+list.push(8)
+list.push(7)
 
-console.log(list.unshift(1))
-console.log(list.head)
+// console.log(list.unshift(1))
+console.log(list.set("rabah", 3))
+// console.log(list.get(3))
