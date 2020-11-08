df1cd9994416a84944980aeb84758b7636993138
Rabah Babaci <4rabah@gmail.com>
2020-11-08 00:09:39 +0000

    implement the extractMax

--- Data-Structures/binaryHeaps.js
+++ Data-Structures/binaryHeaps.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
     constructor() {
-        this.values = [99, 40, 55, 33, 27, 19, 15];
+        this.values = [20, 18, 17, 15, 14, 16, 13, 10, 9, 12, 5];
     }
 
     print() {
@@ -25,10 +25,50 @@ class MaxBinaryHeap {
         this.values.push(element);
         this.bubbleUp();
     }
+
+    sinkDown() {
+        let element = this.values[0], length = this.values.length;
+        let currIndex = 0;
+
+        while (true) {
+            let leftIdx = 2 * currIndex + 1;
+            let rightIdx = 2 * currIndex + 2;
+            let leftChild, rightChild, swap = false;
+
+            if (leftIdx < length) {
+                leftChild = this.values[leftIdx];
+                if (leftChild > element) {
+                    swap = leftIdx;
+                }
+            } if (rightIdx < length) {
+                rightChild = this.values[rightIdx];
+                if ((swap && rightChild > leftChild) || (!swap && rightChild > element)) {
+                    swap = rightIdx;
+                } 
+            }
+
+            if (!swap) break;
+
+            this.values[currIndex] = this.values[swap];
+            this.values[swap] = element;
+            currIndex = swap;
+        }
+    }
+
+    extractMax() {
+        if (!this.values.length) return undefined;
+
+        let max = this.values[0];
+        this.values[0] = this.values.pop();
+
+        this.sinkDown();
+        return max;
+    }
 }
 
 let maxHeap = new MaxBinaryHeap();
 
-maxHeap.insert(39);
+// maxHeap.insert(39);
 
+console.log(maxHeap.extractMax())
 maxHeap.print()
