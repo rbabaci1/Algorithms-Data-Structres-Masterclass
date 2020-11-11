f0bd644393a360eb99b89e8b40ad51fa825b3882
Rabah Babaci <4rabah@gmail.com>
2020-11-11 21:09:06 +0000

    implement the removeVertex method

--- Data-Structures/graph.js
+++ Data-Structures/graph.js
@@ -16,8 +16,11 @@ class Graph {
         }
     }
 
-    removeVertex() {
-        
+    removeVertex(vertex) {
+        if (this.adjancyList[vertex]) {
+            this.adjancyList[vertex].forEach(v => this.removeEdge(vertex, v));
+            delete this.adjancyList[vertex];
+        }
     }
 
     removeEdge(vertex1, vertex2) {
@@ -33,11 +36,15 @@ const graph = new Graph();
 graph.addVertex("oakland");
 graph.addVertex("berkeley");
 graph.addVertex("san_francisco");
+graph.addVertex("san_jose");
 
 graph.addEdge("oakland", "berkeley")
+graph.addEdge("oakland", "san_jose")
 graph.addEdge("oakland", "san_francisco")
-graph.addEdge("oakland", "alice")
+graph.addEdge("san_jose", "berkeley")
 
-graph.removeEdge("oakland", "berkeley")
+// graph.removeEdge("oakland", "berkeley")
 
+console.log(graph.adjancyList);
+graph.removeVertex("san_jose");
 console.log(graph.adjancyList);
