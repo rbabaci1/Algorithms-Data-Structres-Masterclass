ab070df6b4d2ea9f5b5d3c045cad454a305d1c2d
Rabah Babaci <4rabah@gmail.com>
2020-11-11 20:49:46 +0000

    implement the addEdge method

--- Data-Structures/graph.js
+++ Data-Structures/graph.js
@@ -6,12 +6,25 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjancyList[vertex]) {
             this.adjancyList[vertex] = [];
-        } else return "Vertext already exists!";
+        } else return "Specified Vertex already exists.";
+    }
+
+    addEdge(vertex1, vertex2) {
+        if (this.adjancyList[vertex1] && this.adjancyList[vertex2]) {
+            this.adjancyList[vertex1].push(vertex2);
+            this.adjancyList[vertex2].push(vertex1);
+        }
     }
 }
 
 const graph = new Graph();
 
-graph.addVertex("rabah");
+graph.addVertex("oakland");
+graph.addVertex("berkeley");
+graph.addVertex("san_francisco");
+
+graph.addEdge("oakland", "berkeley")
+graph.addEdge("oakland", "san_francisco")
+graph.addEdge("oakland", "alice")
 
 console.log(graph.adjancyList);
