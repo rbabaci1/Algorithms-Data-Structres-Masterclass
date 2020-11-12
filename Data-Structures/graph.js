044bf8aadc2399cc496a838f97eda8a867a458f4
Rabah Babaci <4rabah@gmail.com>
2020-11-12 02:57:58 +0000

    implement the recursive DFT

--- Data-Structures/graph.js
+++ Data-Structures/graph.js
@@ -29,22 +29,47 @@ class Graph {
             this.adjancyList[vertex2] = this.adjancyList[vertex2].filter(v => v !== vertex1);
         }
     }
+
+    getNeighors(vertex) {
+        return this.adjancyList[vertex];
+    }
+
+    DFTRecursive(startingVertex, visited=null) {
+        if (!visited) visited = new Set();
+        visited.add(startingVertex);
+
+        this.adjancyList[startingVertex].forEach(neighbor => {
+            if (!visited.has(neighbor)) {
+                this.DFTRecursive(neighbor, visited);
+            }
+        })
+        return visited;
+    }
 }
 
 const graph = new Graph();
 
-graph.addVertex("oakland");
-graph.addVertex("berkeley");
-graph.addVertex("san_francisco");
-graph.addVertex("san_jose");
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-graph.addEdge("oakland", "berkeley")
-graph.addEdge("oakland", "san_jose")
-graph.addEdge("oakland", "san_francisco")
-graph.addEdge("san_jose", "berkeley")
+graph.addEdge("A", "B")
+graph.addEdge("A", "C")
+graph.addEdge("B", "D")
+graph.addEdge("C", "E")
+graph.addEdge("D", "E")
+graph.addEdge("D", "F")
+graph.addEdge("E", "F")
+graph.addEdge("F", "A")
+graph.addEdge("F", "B")
 
 // graph.removeEdge("oakland", "berkeley")
 
+// console.log(graph.adjancyList);
+// graph.removeVertex("san_jose");
 console.log(graph.adjancyList);
-graph.removeVertex("san_jose");
-console.log(graph.adjancyList);
+
+console.log(graph.DFTRecursive("A"))
