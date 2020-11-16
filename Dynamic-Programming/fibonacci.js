f0c50af11d8cb67cd9b2dd3df17fc4046c41dbeb
Rabah Babaci <4rabah@gmail.com>
2020-11-16 03:20:05 +0000

    .

--- Dynamic-Programming/fibonacci.js
+++ Dynamic-Programming/fibonacci.js
@@ -1,12 +1,24 @@
-function memoizedFib(num, memo=null) {
+// Top - Down approach
+function memoizedFib1(num, memo=null) {
     if (num <= 2) return 1;
     if (!memo) memo = {};
 
     if (!memo[num]) {
-        memo[num] = (memoizedFib(num - 1, memo) + memoizedFib(num - 2, memo));
+        memo[num] = (memoizedFib1(num - 1, memo) + memoizedFib1(num - 2, memo));
     }
     return memo[num];
 }
 
+// Bottom - Up approach
+function memoizedFib2(num) {
+    let fibs = [0, 1, 1];
 
-console.log(memoizedFib(40));
+    for (let i = 3; i <= num; i++) {
+        let calc = fibs[i - 1] + fibs[i - 2];
+        fibs.push(calc);
+    }
+    return fibs[num];
+}
+
+
+console.log(memoizedFib2(45));
